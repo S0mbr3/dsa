31743aed870a057a5f5e859bc754e6e7a59790b9
S0mbr3 <benjamin_denis@outlook.com>
2023-09-05 11:43:49 +0200

    Implementation of bubble_sort, two crystal balls algorithms and LinkedList interface

--- compiled/sort/bubble_sort.js
+++ compiled/sort/bubble_sort.js
@@ -1,4 +1,13 @@
 "use strict";
+/*The bubble sort is a simple sorting algorithm, it just check if the next number in the list is superior if it does, swap with the previous one until the last swapped number and start again from the begining until the first element:
+  As we can see the time complexity is equivalent of adding each number of a range which can be represented with this formula n(n + 1) / 2 which give a  Big O of n2
+  Ex: 1 .. 5 -> 1+2+3+4+5 = 5 * ( 5 + 1) / 2
+  adding each number in a range by adding their limit together until we reach the middle
+  1 .. 5 -> 1 + 5 = 6
+  2 .. 4 -> 2 + 4 = 6
+  3 .. 3 - > 3 + 3 = 6
+  then 6 * 3 = 15
+  We then can generalize n(n + 1) / 2*/
 Object.defineProperty(exports, "__esModule", { value: true });
 function bubble_sort(list) {
     for (let i = 0; i < list.length; ++i) {
